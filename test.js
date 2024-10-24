79b6881b482ff81f2b3682eaafea5fb00a487879
Bill Byte <bill.byte@pm.me>
2024-10-24 07:39:28 +0200

    up

--- test.js
+++ test.js
@@ -1,24 +1,38 @@
-
-const buttonRegister = document.querySelector('.basicLightbox .confirm-button"]');
-if (buttonRegister !== null) {
-    const mail = document.querySelector('#newsletter .newsletter-input input[type="text"]');
-    const box = document.querySelector('.basicLightbox input[type="checkbox"]');
-    document.addEventListener("keydown", event => {
-        if (event.key === "Enter") {
-            if (box.checked) {
-                _edrone.email = mail.value;
-                _edrone.customer_tags = 'test';
-                _edrone.action_type = 'subscribe';
-                _edrone.init();
-            }
-        }
-    })
-    buttonRegister.addEventListener('click', () => {
-        if (box.checked) {
-            _edrone.email = mail.value;
-            _edrone.customer_tags = 'test';
-            _edrone.action_type = 'subscribe';
-            _edrone.init();
-        }
-    })
-}
+!(function () {
+	document.querySelector('body').addEventListener('mousedown', function (e) {
+		var containerName = '#add-to-cart-or-refresh'
+		var addToCartButtonName = '.add-to-cart'
+		var isProductPage = true
+		var productIdName = ''
+		var productAttributeName = ''
+		var productContainerList = document.querySelectorAll(containerName)
+		if (productContainerList.length !== 0) {
+			productContainerList.forEach(function (container, index) {
+				var addToCartButton = container.querySelector(addToCartButtonName)
+				var productId =
+					!isProductPage === true
+						? container.querySelector(productIdName)
+						: null
+				if (addToCartButton) {
+					var eventPath = e.path || e.composedPath()
+					if (eventPath.includes(addToCartButton)) {
+						if (isProductPage || productId) {
+							if (!isProductPage) {
+								_edrone.product_category_ids = ''
+								_edrone.product_category_names = ''
+								_edrone.product_titles = ''
+								_edrone.product_urls = ''
+								_edrone.product_images = ''
+								_edrone.product_ids =
+									productId.getAttribute(productAttributeName)
+								_edrone.product_skus = _edrone.product_skus
+							}
+							_edrone.action_type = 'add_to_cart'
+							_edrone.init()
+						}
+					}
+				}
+			})
+		}
+	})
+})()

3c36057e16457d0a9bb4b042b9e014e341221944
Bill Byte <bill.byte@pm.me>
2025-03-04 20:52:26 +0100

    up

--- edroneAddToCart.js
+++ edroneAddToCart.js
@@ -1,20 +1,41 @@
 // add_to_cart homepage with pathname
 
-const currentPathname = window.location.pathname
-var xxx = 'ddd'
-if (currentPathname === '/') {
-	const productsContainer = document.querySelectorAll('.product-miniature')
-	if (productsContainer !== null) {
-		[].forEach.call(productsContainer, (el) => {
-			const addToBasketBtn = '.add-to-cart'
-			if (el.querySelector(addToBasketBtn) !== null) {
-				el.querySelector(addToBasketBtn).addEventListener('click', () => {
-					const prodId = el.getAttribute('data-id-product')
-					_edrone.product_ids = prodId
-					_edrone.action_type = 'add_to_cart'
-					_edrone.init()
-				})
-			}
-		})
-	}
+// const currentPathname = window.location.pathname
+// var xxx = 'ddd'
+// if (currentPathname === '/') {
+const productsContainer = document.querySelectorAll('.product-miniature')
+if (productsContainer !== null) {
+	;[].forEach.call(productsContainer, (el) => {
+		const addToBasketBtn = el.querySelector('.add-to-cart');
+		if (addToBasketBtn !== null) {
+			addToBasketBtn.addEventListener('click', () => {
+				const prodId = el.getAttribute('data-id-product')
+				// or
+				// const prodId = el.querySelector('input[name="id_product"]');
+				// _edrone.product_ids = prodId.getAttribute('value');
+				_edrone.product_ids = prodId
+				_edrone.action_type = 'add_to_cart'
+				_edrone.init()
+			})
+		}
+	})
 }
+// }
+
+//if nothing else works :D
+!(function () {
+	document.querySelector('.addtocart').addEventListener('mousedown', function (e) {
+		var containerSelector = '.products-grid .item'
+		var productIdName = '.item'
+		var productAttributeName = 'data-product-id'
+		var addToCartButton = e.target.closest(productIdName)
+		if (!addToCartButton) return
+		var productContainer = addToCartButton.closest(containerSelector)
+		if (!productContainer) return
+		var productId = productContainer.getAttribute(productAttributeName)
+		if (!productId) return
+		_edrone.product_skus = productId
+		_edrone.action_type = 'add_to_cart'
+		_edrone.init()
+	})
+})()

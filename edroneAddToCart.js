acfbc0ee820ea4ec96887cc5bbde7cf608d44ec4
Bill Byte <bill.byte@pm.me>
2025-10-01 12:41:09 +0200

    up

--- edroneAddToCart.js
+++ edroneAddToCart.js
@@ -24,18 +24,16 @@ if (productsContainer !== null) {
 
 //if nothing else works :D
 !(function () {
-	document.querySelector('.addtocart').addEventListener('mousedown', function (e) {
-		var containerSelector = '.products-grid .item'
-		var productIdName = '.item'
-		var productAttributeName = 'data-product-id'
-		var addToCartButton = e.target.closest(productIdName)
-		if (!addToCartButton) return
-		var productContainer = addToCartButton.closest(containerSelector)
-		if (!productContainer) return
-		var productId = productContainer.getAttribute(productAttributeName)
-		if (!productId) return
-		_edrone.product_skus = productId
-		_edrone.action_type = 'add_to_cart'
-		_edrone.init()
-	})
-})()
+  document.querySelectorAll('.custom-discount-form button.btn.btn-red') // add to cart button
+    .forEach(function (btn) {
+      btn.addEventListener('mousedown', function (e) {
+        var productContainer = e.target.closest('.product');
+        if (!productContainer) return;
+        var productId = productContainer.getAttribute('data-product-id');
+        if (!productId) return;
+        _edrone.product_skus = productId;
+        _edrone.action_type = 'add_to_cart';
+        _edrone.init();
+      });
+    });
+})();
